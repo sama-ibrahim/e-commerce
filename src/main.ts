6b9115e23e96a335ec78bfde65225d3f90c0b72d
sama <sama.edrahim@gmail.com>
2026-03-25 23:48:17 +0200

    interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-
- 
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor, TransformInterceptor } from '@common/interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new TransformInterceptor(),
+  );
 
   app.enableCors({
-    origin:"*",
-    
-  })
-  app.useGlobalPipes(new ValidationPipe())
- //app.useGlobalFilters(new HttpExceptionFilter());
+    origin: '*',
+  });
+
+  //app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
